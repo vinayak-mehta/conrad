7d1dba8460c0a262855bb6eff9f5fd2972c4917a
Krishna Prasanth Guttula <krishnaprasanth.guttula@colruytgroup.com>
2019-10-30 14:13:16 +0100

    fix linting

--- conrad/utils.py
+++ conrad/utils.py
@@ -2,11 +2,13 @@
 
 import json
 import os
-import operator
+import operator as op
 from jsonschema import Draft4Validator
 
 from .db import engine
 
+schema_name = "default_event_schema.json"
+schema_path = os.path.join(os.path.dirname(__file__), schema_name)
 
 def initialize_database():
     from .models import Base
@@ -22,18 +24,19 @@ def reset_database():
 
 
 def validate(input_events):
-    failures = []
-    with open(os.path.join(os.path.dirname(__file__), "default_event_schema.json")) as f:
-        schema = json.load(f)
+    with open(schema_path) as file_h:
+        schema = json.load(file_h)
     validator = Draft4Validator(schema)
 
-    _errors = sorted(validator.iter_errors(input_events), key=operator.attrgetter('path'))
+    _errors = sorted(validator.iter_errors(input_events), key=op.attrgetter('path'))
     error_messages = []
     for err in _errors:
         err_msg = []
-        err_msg.append("[%s] -> %s" % ("][".join(repr(index)
-                                                 for index in err.absolute_path), err.message))
-        for suberror in sorted(err.context, key=operator.attrgetter('schema_path')):
+        err_msg.append("[%s] -> %s" % (
+            "][".join(repr(index)
+            for index in err.absolute_path), err.message))
+
+        for suberror in sorted(err.context, key=op.attrgetter('schema_path')):
             err_msg.append("  %s" % suberror.message)
 
         error_messages.append("\n".join(err_msg))
