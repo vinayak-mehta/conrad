68e2d50689f85d2c84ebbf56aa8d49f9302766fb
Vinayak Mehta <vmehta94@gmail.com>
2019-11-05 20:26:53 +0530

    Parse talk deadline

--- crawlers/pycon/pycon_crawler.py
+++ crawlers/pycon/pycon_crawler.py
@@ -11,8 +11,12 @@ from ..base import BaseCrawler
 class PyConCrawler(BaseCrawler):
     def get_events(self):
         df = pandas.read_csv(
-            "https://raw.githubusercontent.com/python-organizers/conferences/master/2020.csv"
+            "https://raw.githubusercontent.com/python-organizers/conferences/master/2020.csv",
+            quoting=1,
+            encoding="utf-8",
+            dtype=str,
         )
+        df = df.fillna("")
 
         for event in df.to_dict(orient="records"):
             location = event["Location"].split(",")
@@ -25,15 +29,23 @@ class PyConCrawler(BaseCrawler):
                 state = location[1].strip()
                 country = event["Country"]
 
+            cfp_end_date = (
+                event["Talk Deadline"] if event["Talk Deadline"] else "1970-01-01"
+            )
+            cfp_open = (
+                True
+                if dt.datetime.now() < dt.datetime.strptime(cfp_end_date, "%Y-%m-%d")
+                else False
+            )
             e = {
                 "name": event["Subject"],
                 "url": event["Website URL"],
                 "city": city,
                 "state": state,
                 "country": country,
-                "cfp_open": False,
+                "cfp_open": cfp_open,
                 "cfp_start_date": "1970-01-01",
-                "cfp_end_date": "1970-01-01",
+                "cfp_end_date": cfp_end_date,
                 "start_date": event["Start Date"],
                 "end_date": event["End Date"],
                 "source": "https://github.com/python-organizers/conferences",
