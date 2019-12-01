5c527f4816ee3d848fbfe1b39d21f4a2ed79b6ee
Vinayak Mehta <vmehta94@gmail.com>
2019-12-02 00:56:30 +0530

    Add awesome italy events crawler

--- crawlers/__init__.py
+++ crawlers/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from .italy.italy_crawler import ItalyCrawler
 from .pycon.pycon_crawler import PyConCrawler
 from .pydata.pydata_crawler import PyDataCrawler
 from .papercall.papercall_crawler import PapercallCrawler
---
+++ crawlers/italy/italy_crawler.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import json
+import datetime as dt
+
+import requests
+
+from ..base import BaseCrawler
+
+
+class ItalyCrawler(BaseCrawler):
+    def get_events(self):
+        response = requests.get(
+            "https://raw.githubusercontent.com/ildoc/awesome-italy-events/master/data/2020.json"
+        )
+
+        for event in json.loads(response.content):
+            if "pycon" in event["title"].lower():
+                continue
+
+            e = {
+                "name": event["title"],
+                "url": event["url"],
+                "city": event["location"],
+                "state": None,
+                "country": "Italy",
+                "cfp_open": False,
+                "cfp_end_date": "1970-01-01",
+                "start_date": event["startDate"],
+                "end_date": event["endDate"],
+                "source": "https://github.com/ildoc/awesome-italy-events",
+                "tags": ["technology"],
+                "kind": "conference",
+                "by": "bot",
+            }
+            self.events.append(e)
