9185b1a134e5a9eb490281dd43f3e02bf3b5fc0b
kunaljubce <kunal.jubce@gmail.com>
2020-04-05 01:31:58 +0530

    Added code to return city, state and country data. Also to capture and parse data for online conferences

--- crawlers/depodcast/depodcast_crawler.py
+++ crawlers/depodcast/depodcast_crawler.py
@@ -8,6 +8,21 @@ from ..base import BaseCrawler
 
 class DEPodcastCrawler(BaseCrawler):
 
+    def parse_date(self, event_dates):
+        '''
+        Function to parse date ranges and return the start and end dates
+        '''
+        if '-' in event_dates:
+            start_dt = event_dates.split(' - ')[0]
+            start_dt_formatted = datetime.strftime(parse(start_dt), '%Y-%m-%d')
+            month = start_dt.split(' ')[0]
+            end_dt = event_dates.split(' - ')[1] + ' ' +  month
+            end_dt_formatted = datetime.strftime(parse(end_dt), '%Y-%m-%d')
+        else:
+            start_dt_formatted = datetime.strftime(parse(event_dates), '%Y-%m-%d')
+            end_dt_formatted = datetime.strftime(parse(event_dates), '%Y-%m-%d')
+        return (start_dt_formatted, end_dt_formatted)
+
     def get_events(self):
         geolocator = Nominatim(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36")
         URL = 'https://www.dataengineeringpodcast.com/conferences/'
@@ -18,32 +33,40 @@ class DEPodcastCrawler(BaseCrawler):
         for conf in conf_list:
             title = conf.find('h2', class_='elementor-heading-title elementor-size-default')
             place = conf.find('div', class_='elementor-custom-embed')
-            if title is not None and "Subscribe" not in title.text and place is not None:
-                event_info = title.text
-                hyp_index = event_info.index(' - ')
-                event_name_and_loc = event_info[:hyp_index]
-                event_dates = event_info[hyp_index+3:]
-                start_dt = event_dates.split(' - ')[0]
-                start_dt_formatted = datetime.strftime(parse(start_dt), '%Y-%m-%d')
-                month = start_dt.split(' ')[0]
-                end_dt = event_dates.split(' - ')[1] + ' ' +  month
-                end_dt_formatted = datetime.strftime(parse(end_dt), '%Y-%m-%d')
-                event_link = conf.find('a')['href']
-                place = place.iframe.extract().attrs['aria-label']
-                if "Convention Center" in place:
-                    pattern = re.compile(r'[a-zA-z\.\- ]*Convention Center')
-                    match = pattern.finditer(place)
-                    for m in match:
-                        place = m.group()
-                complete_addr = geolocator.geocode(place).address
-                last_comma = complete_addr.rfind(',')
-                country = complete_addr[(last_comma+1):].strip()
-
+            city = 'Online'
+            state = ''
+            country = 'Online'
+            if title is not None and "Subscribe" not in title.text:
+                if place is not None:
+                    event_info = title.text
+                    hyp_index = event_info.index(' - ')
+                    event_name_and_loc = event_info[:hyp_index]
+                    event_dates = event_info[hyp_index+3:]
+                    start_dt_formatted, end_dt_formatted = self.parse_date(event_dates)
+                    event_link = conf.find('a')['href']
+                    place = place.iframe.extract().attrs['aria-label']
+                    if "Convention Center" in place:
+                        pattern = re.compile(r'[a-zA-z\.\- ]*Convention Center')
+                        match = pattern.finditer(place)
+                        for m in match:
+                            place = m.group()
+                    complete_addr = geolocator.geocode(place).address
+                    city = complete_addr.split(',')[-5].strip()
+                    state = complete_addr.split(',')[-3].strip()
+                    country = complete_addr.split(',')[-1].strip()
+                else:
+                    event_info = title.text
+                    hyp_index = event_info.index(' - ')
+                    event_name_and_loc = event_info[:hyp_index]
+                    event_dates = event_info[hyp_index+3:]
+                    start_dt_formatted, end_dt_formatted = self.parse_date(event_dates)
+                    event_link = conf.find('a')['href']
+                
                 e = {
                     "name": event_name_and_loc,
                     "url": event_link,
-                    "city": place,
-                    "state": None,
+                    "city": city,
+                    "state": state,
                     "country": country,
                     "cfp_open": False,
                     "cfp_end_date": "1970-01-01",
@@ -54,5 +77,5 @@ class DEPodcastCrawler(BaseCrawler):
                     "kind": "conference",
                     "by": "bot",
                 }
-
+                #print(e)
                 self.events.append(e)
