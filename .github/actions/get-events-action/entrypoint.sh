edc03a38f7855288a95e48fab17a281fb46b2d77
Vinayak Mehta <vmehta94@gmail.com>
2020-07-30 18:24:23 +0530

    Remove conrad from crawl requirements

--- .github/actions/get-events-action/entrypoint.sh
+++ .github/actions/get-events-action/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 
 pip install -r requirements-crawl.txt
+pip install .
+
 python -m crawlers $1
 
 FILENAME="data/`echo $1 | awk '{print tolower($0)}' | sed -e 's/crawler//g'`_events.json"
