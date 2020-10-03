e782134a0775b65430bae9568e96ea057a6c299e
Divya <virmani.divya18@gmail.com>
2020-10-02 22:25:09 -0700

    issue_146

--- crawlers/italy/italy_crawler.py
+++ crawlers/italy/italy_crawler.py
@@ -5,32 +5,52 @@ import json
 import requests
 
 from ..base import BaseCrawler
+from datetime import datetime
 
 
 class ItalyCrawler(BaseCrawler):
-    def get_events(self):
-        response = requests.get(
-            "https://raw.githubusercontent.com/ildoc/awesome-italy-events/master/data/2020.json"
-        )
 
+    def get_events(self):
+        current_year = datetime.now().year        
+        use_year = current_year
+
+        while self.get_data_for_year(use_year):
+            use_year += 1
+
+    def get_data_for_year (self, year):
+
+        response = requests.get(
+            "https://raw.githubusercontent.com/ildoc/awesome-italy-events/master/data/" + str(year) + ".json"
+        )
+        
+        if response.status_code != 200:
+            return False
+        
+        current_date = datetime.date(datetime.now())
         for event in json.loads(response.content):
+
             if "pycon" in event["title"].lower():
                 continue
+            
+            conference_date = datetime.strptime(event["endDate"], '%Y-%m-%d').date()
+            if conference_date >= current_date:
+                e = {
+                    "name": event["title"],
+                    "url": event["url"],
+                    "city": event["location"],
+                    "state": None,
+                    "country": "Italy",
+                    "location": event["location"],
+                    "cfp_open": False,
+                    "cfp_end_date": "1970-01-01",
+                    "start_date": event["startDate"],
+                    "end_date": event["endDate"],
+                    "source": "https://github.com/ildoc/awesome-italy-events",
+                    "tags": ["technology"],
+                    "kind": "conference",
+                    "by": "bot",
+                }
+                self.events.append(e)
+        return True
+        
 
-            e = {
-                "name": event["title"],
-                "url": event["url"],
-                "city": event["location"],
-                "state": None,
-                "country": "Italy",
-                "location": event["location"],
-                "cfp_open": False,
-                "cfp_end_date": "1970-01-01",
-                "start_date": event["startDate"],
-                "end_date": event["endDate"],
-                "source": "https://github.com/ildoc/awesome-italy-events",
-                "tags": ["technology"],
-                "kind": "conference",
-                "by": "bot",
-            }
-            self.events.append(e)
--- crawlers/pycon/pycon_crawler.py
+++ crawlers/pycon/pycon_crawler.py
@@ -6,51 +6,70 @@ import datetime as dt
 import pandas
 
 from ..base import BaseCrawler
+from datetime import datetime
+import requests
 
 
 class PyConCrawler(BaseCrawler):
     def get_events(self):
-        df = pandas.read_csv(
-            "https://raw.githubusercontent.com/python-organizers/conferences/master/2020.csv",
-            quoting=1,
-            encoding="utf-8",
-            dtype=str,
-        )
-        df = df.fillna("")
+        current_year = datetime.now().year        
+        use_year = current_year
+        while self.get_data_for_year(use_year):
+            use_year += 1
 
-        for event in df.to_dict(orient="records"):
-            location = event["Location"].split(",")
-            city = state = country = None
-            if len(location) == 2:
-                city = location[0].strip()
-                country = location[1].strip()
-            elif len(location) == 3:
-                city = location[0].strip()
-                state = location[1].strip()
-                country = event["Country"]
+    def get_data_for_year (self, year):
+        try:
+            df = pandas.read_csv(
+                "https://raw.githubusercontent.com/python-organizers/conferences/master/" + str(year) + ".csv",
+                quoting=1,
+                encoding="utf-8",
+                dtype=str,
+            )
 
-            cfp_end_date = (
-                event["Talk Deadline"] if event["Talk Deadline"] else "1970-01-01"
-            )
-            cfp_open = (
-                True
-                if dt.datetime.now() < dt.datetime.strptime(cfp_end_date, "%Y-%m-%d")
-                else False
-            )
-            e = {
-                "name": event["Subject"],
-                "url": event["Website URL"],
-                "city": city,
-                "state": state,
-                "country": country,
-                "location": ", ".join(filter(lambda x: x is not None, [city, state, country])),
-                "cfp_open": cfp_open,
-                "cfp_end_date": cfp_end_date,
-                "start_date": event["Start Date"],
-                "end_date": event["End Date"],
-                "source": "https://github.com/python-organizers/conferences",
-                "tags": ["python"],
-                "kind": "conference",
-                "by": "bot",
-            }
-            self.events.append(e)
+            df = df.fillna("")
+
+            current_date = datetime.date(datetime.now())
+
+            for event in df.to_dict(orient="records"):
+                conference_date = datetime.strptime(event["End Date"], '%Y-%m-%d').date()
+
+                if conference_date >= current_date:
+                    location = event["Location"].split(",")
+                    city = state = country = None
+                    if len(location) == 2:
+                        city = location[0].strip()
+                        country = location[1].strip()
+                    elif len(location) == 3:
+                        city = location[0].strip()
+                        state = location[1].strip()
+                        country = event["Country"]
+
+                    cfp_end_date = (
+                        event["Talk Deadline"] if event["Talk Deadline"] else "1970-01-01"
+                    )
+                    cfp_open = (
+                        True
+                        if dt.datetime.now() < dt.datetime.strptime(cfp_end_date, "%Y-%m-%d")
+                        else False
+                    )
+                    e = {
+                        "name": event["Subject"],
+                        "url": event["Website URL"],
+                        "city": city,
+                        "state": state,
+                        "country": country,
+                        "location": ", ".join(filter(lambda x: x is not None, [city, state, country])),
+                        "cfp_open": cfp_open,
+                        "cfp_end_date": cfp_end_date,
+                        "start_date": event["Start Date"],
+                        "end_date": event["End Date"],
+                        "source": "https://github.com/python-organizers/conferences",
+                        "tags": ["python"],
+                        "kind": "conference",
+                        "by": "bot",
+                    }
+                    self.events.append(e)
+            return True
+        except:
+            return False
+
