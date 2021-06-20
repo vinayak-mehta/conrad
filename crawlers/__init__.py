ec6e2bcf3338d5eb3716595aa8c072069a1548c5
Vinayak Mehta <vmehta94@gmail.com>
2021-06-20 21:51:22 +0530

    Remove unused crawlers

--- crawlers/__init__.py
+++ crawlers/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from .italy.italy_crawler import ItalyCrawler
 from .pycon.pycon_crawler import PyConCrawler
 from .python.python_crawler import PythonCrawler
 from .pydata.pydata_crawler import PyDataCrawler
-from .papercall.papercall_crawler import PapercallCrawler
